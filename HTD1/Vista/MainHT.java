9dfea2f2384fbaa082f69f6296aba571a3db55bb
angelargd8 <angelargd8@outlook.com>
2023-01-13 12:18:50 -0600

    creación del archivo

---
+++ HTD1/Vista/MainHT.java
@@ -0,0 +1,11 @@
+package Vista;
+
+public class MainHT {
+    /** 
+     * @param args
+     */
+    public static void main(String[] args) {
+        //MostrarMenu mostrarMenu= new MostrarMenu();
+        //mostrarMenu.MostrarPrograma();
+    }
+}
