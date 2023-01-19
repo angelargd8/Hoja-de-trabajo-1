2d2c6fe08cea6ba472d63764fd88b0ec52047a37
angelargd8 <angelargd8@outlook.com>
2023-01-18 18:19:10 -0600

    interfaz de la clase

--- HTD1/CModelo/IRadio.java
+++ HTD1/CModelo/IRadio.java
@@ -1,9 +1,20 @@
-package CModelo;
+public interface IRadio{
 
-public interface IRadio {
+    //Tiene que iniciar apagada y funciona para cambiar la radio de encendida a apagada
     public boolean power();
-    public String cambiarEstacion();
-    public void cambiarEmisora(double cambio);
+
+    //Treue es Fm y false AM, cuando se llama a la funcion esta cambia entre true o false
+    public boolean frecuencia();
+
+    //True es para arriba y false es para abajo.
+    public void cambiarEmisora(boolean subirBajar);
+
+    //obtener la emisora actual
+    public double emisoraActual();
+
+    //guardar la emisora en un ArrayList es el cual solo guarda al final de la lista la emisora
     public void guardarEmisora(double emisora);
-    public double emisoraGuardada(int posicion);
+
+    //Se le manda la posicion en la que se encuentra la emisora seleccionada
+    public void cargarEmisoraGuardada(int posicion);
 }
