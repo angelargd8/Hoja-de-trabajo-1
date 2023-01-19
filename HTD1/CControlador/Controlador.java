1e1b303c5d87f4bc143e2a0f784f383b96c95d39
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-01-18 20:16:58 -0600

    Update Controlador.java

--- HTD1/CControlador/Controlador.java
+++ HTD1/CControlador/Controlador.java
@@ -31,7 +31,42 @@ public class Controlador implements IRadio  {
     @Override
     public void cambiarEmisora(boolean subirBajar) {
         // TODO Auto-generated method stub
-        
+          if(ModulacionDeOnda.equals("AM")){
+            System.out.println("Ingrese 1 para subir de estación o 0 para bajar de estación");
+            int des = in.nextInt();
+            
+            if(des == 1){
+                if( emisoras > 1610){
+                    System.out.println(" Ya no se puede subir más de emisora");
+                } else if ( emisoras <= 1610){
+                    emisoras = emisoras + 10;
+                }
+                
+            } else if(des == 0){
+                if(emisoras < 530 ){
+                    System.out.println("Ya no es posible bajar de emisora");
+                } else if( emisoras >= 530)
+                emisoras = emisoras - 10;
+            }
+        } else if (ModulacionDeOnda.equals("FM")){
+            System.out.println("Ingrese 1 para subir de estación o 0 para bajar de estación");
+            int des = in.nextInt();
+
+            if(des == 1){
+                if( emisoras > 107.9){
+                    System.out.println(" Ya no se puede subir más de emisora");
+                } else if ( emisoras <= 107.9){
+                    emisoras = emisoras + 0.2;
+                }
+                
+            } else if(des == 0){
+                if(emisoras < 87.9 ){
+                    System.out.println("Ya no es posible bajar de emisora");
+                } else if( emisoras >= 87.9)
+                emisoras = emisoras - 0.2;
+            }
+
+        }
     }
 
     @Override
