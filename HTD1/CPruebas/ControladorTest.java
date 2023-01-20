69e04ce5d837aa3221e854ff34ff98b2b2851695
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2023-01-19 20:42:03 -0600

    Add files via upload

--- HTD1/CPruebas/ControladorTest.java
+++ HTD1/CPruebas/ControladorTest.java
@@ -1,1 +1,26 @@
+package CPruebas;
 
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+import CControlador.*;
+
+public class ControladorTest {
+
+    private Controlador controlador;
+
+    public void escenario(){
+        controlador = new Controlador();
+    }
+
+    @Test
+    public void testPower(){
+        escenario();
+        assertTrue(controlador.power() == true);
+    }
+
+    @Test
+    public void testFrecuencia(){
+        escenario();
+        assertTrue(controlador.sFrecuencia() == "AM");
+    }
+}
